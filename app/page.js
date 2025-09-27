9d5f1b76ad38f28e1aeb9032d31062b68cc33fdf
Ehsan Allahi <ehsanallahi47@gmail.com>
2025-09-27 20:47:50 +0500

    update Navbar

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-import HeroSection from "@/components/hero-section";
+import { Navbar } from "@/components/header";
+
 
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <HeroSection />
+      <Navbar />
     </div>
   );
 }
